d564623cbec7d7f741f7e10fb79d816de477fc66
Dimitri Stépaniak <dimitri.stepaniak@gmail.com>
2020-02-05 16:22:58 +0100

    feat : Interface Graphique

--- app/src/main/java/magic_book/Main.java
+++ app/src/main/java/magic_book/Main.java
@@ -8,11 +8,17 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.geometry.Insets;
+import javafx.scene.Cursor;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class Main extends Application {
     private String mode;
@@ -21,7 +27,7 @@ public class Main extends Application {
     public void start(Stage stage) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        FlowPane root = new FlowPane();
+        BorderPane border = new BorderPane();
         //Button button = new Button("Bouton");
         //Label l= new Label("0");
         
@@ -32,7 +38,7 @@ public class Main extends Application {
         //root.getChildren().add(button);
         //root.getChildren().add(l);
         mode = "0";
-        Scene scene = new Scene(root,600,600);
+        Scene scene = new Scene(border,1000,800);
         
         Button add = new Button("Ajouter");
         Button del = new Button("Supprimer");
@@ -51,16 +57,36 @@ public class Main extends Application {
            System.out.println("clic bouton");
         });
         
-        root.addEventFilter(MouseEvent.MOUSE_PRESSED,(new EventHandler<MouseEvent>() {
+        add.setMinSize(100,100);
+        add.setMaxSize(100,100);
+        del.setMinSize(100,100);
+        del.setMaxSize(100,100);
+        mod.setMinSize(100,100);
+        mod.setMaxSize(100,100);
+        
+        FlowPane flow = new FlowPane();
+        flow.getChildren().addAll(add,del,mod);
+        flow.setMaxWidth(210);
+        flow.setPadding(new Insets(5, 5, 5, 5));
+        border.setLeft(flow);
+        System.out.println(border.getCenter());
+        Pane appContent = new Pane();
+        appContent.setCursor(Cursor.CLOSED_HAND);
+        border.setCenter(appContent);
+        
+        appContent.addEventFilter(MouseEvent.MOUSE_PRESSED,(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
                 System.out.println("clic fenêtre avec mode "+mode);
+                if (mode=="add"){
+                    
                 }
-            }));
-        
-        root.getChildren().addAll(add,del,mod);
-        
-        
+                if (mode=="del"){
+                    
+                }
+            }
+        }));
+          
         stage.setScene(scene);
         stage.show();
         
