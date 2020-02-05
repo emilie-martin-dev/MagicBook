1ae40b094f5f1859fd1069758a7b8e111de08ef1
aure <21806986@unicaen.fr>
2020-02-05 17:02:13 +0100

    feat : ajout de NodeLinkDialog - non fini

--- app/src/main/java/magic_book/Main.java
+++ app/src/main/java/magic_book/Main.java
@@ -7,14 +7,19 @@ import javafx.stage.Stage;
 import magic_book.core.node.Node;
 import magic_book.core.node.NodeType;
 import magic_book.window.graph.NodeDialog;
+import magic_book.window.graph.NodeLinkDialog;
 
 public class Main extends Application {
 
 	public void start(Stage stage) {
-		Node node = new Node("test",NodeType.FAILURE,null);
+		//stage.show();
+		//Node node = new Node("test",NodeType.FAILURE,null);
 		//NodeDialog dialog = new NodeDialog();
-		NodeDialog dialog = new NodeDialog(node);
-		System.out.println("Le texte creer : "+node);
+		//NodeDialog dialog = new NodeDialog(node);
+		//System.out.println("Le texte creer : "+node);
+		
+		NodeLinkDialog linkDialog = new NodeLinkDialog();
+		System.out.println("Le texte creer : "+linkDialog);
 		}
 	
 	public static void main(String[] args) {
