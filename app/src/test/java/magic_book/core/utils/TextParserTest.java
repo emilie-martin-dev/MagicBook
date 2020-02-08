96be5cd61e0ed5f488c3df908e521a23849f91ec
Justine Martin <justine.martin.dev@gmail.com>
2020-02-08 09:44:29 +0100

    test fix : Change le constructeur des Character
    Le constructeur de Character ayant changé, les tests ont été mis à jour

--- app/src/test/java/magic_book/core/utils/TextParserTest.java
+++ app/src/test/java/magic_book/core/utils/TextParserTest.java
@@ -15,10 +15,10 @@ public class TextParserTest {
 		String text = "## c ## est arrivée à bon port avec le {{ i1 }}. Le messager de ## c ##, ##  c2     ## en compagnie de ##c3## se tiennent devant vous. \\#\\# Pas bouger! \\#\\#";
 		String expected = "Jennifer est arrivée à bon port avec le Sceptre. Le messager de Jennifer, Zeref en compagnie de Pluton se tiennent devant vous. \\#\\# Pas bouger! \\#\\#";
 		
-		Character c1 = new Character("c", "Jennifer", "", 1, 1, 1, null, null, 1);
-		Character c2 = new Character("c2", "Zeref", "", 1, 1, 1, null, null, 1);
-		Character c3 = new Character("c3", "Pluton", "", 1, 1, 1, null, null, 1);
-		Character c4 = new Character("c4", "Error", "", 1, 1, 1, null, null, 1);
+		Character c1 = new Character("c", "Jennifer", "", 1, 1, null, null, 1);
+		Character c2 = new Character("c2", "Zeref", "", 1, 1, null, null, 1);
+		Character c3 = new Character("c3", "Pluton", "", 1, 1, null, null, 1);
+		Character c4 = new Character("c4", "Error", "", 1, 1, null, null, 1);
 		Item i = new Item("i1", "Sceptre");
 				
 		Assert.assertEquals(expected, TextParser.parseText(text, Arrays.asList(i), Arrays.asList(c1, c2, c3, c4)));
