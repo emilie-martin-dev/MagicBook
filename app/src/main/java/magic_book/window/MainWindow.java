927b24395d4d0b03df1d79f19dbbfdbf904f685a
Justine Martin <justine.martin.dev@gmail.com>
2020-02-17 22:38:30 +0100

    feat : Ajoute un menu à la MainWindow

--- app/src/main/java/magic_book/window/MainWindow.java
+++ app/src/main/java/magic_book/window/MainWindow.java
@@ -6,6 +6,10 @@ import javafx.geometry.Insets;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
@@ -15,17 +19,17 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class MainWindow extends Stage {
-	
+
 	enum Mode {
 		SELECT, ADD_NODE, ADD_NODE_LINK;
 	}
-	
+
 	private Mode mode;
 	private ToggleGroup toggleGroup;
 
 	public MainWindow() {
 		BorderPane root = new BorderPane();
-		
+
 		Pane mainContent = new Pane();
 		mainContent.setCursor(Cursor.CLOSED_HAND);
 		mainContent.addEventHandler(MouseEvent.MOUSE_PRESSED, (new EventHandler<MouseEvent>() {
@@ -36,50 +40,85 @@ public class MainWindow extends Stage {
 				}
 			}
 		}));
-		
+
+		root.setTop(createMenuBar());
 		root.setLeft(createLeftPanel());
 		root.setCenter(mainContent);
-		
+
 		Scene scene = new Scene(root, 1000, 800);
 
 		this.setTitle("Magic Book");
 		this.setScene(scene);
 		this.show();
 	}
-	
+
+	private MenuBar createMenuBar() {
+		MenuBar menuBar = new MenuBar();
+
+		// --- Menu fichier
+		Menu menuFile = new Menu("Fichier");
+		MenuItem menuFileNew = new MenuItem("Nouveau");
+		MenuItem menuFileOpen = new MenuItem("Ouvrir");
+		MenuItem menuFileSave = new MenuItem("Enregistrer");
+		MenuItem menuFileSaveAs = new MenuItem("Enregistrer sous");
+
+		menuFile.getItems().addAll(menuFileNew, menuFileOpen, menuFileSave, menuFileSaveAs);
+
+		// --- Menu livre
+		Menu menuBook = new Menu("Livre");
+		MenuItem menuBookDifficulty = new MenuItem("Estimer la difficulté");
+		MenuItem menuBookGenerate = new MenuItem("Générer le livre");
+
+		menuBook.getItems().addAll(menuBookDifficulty, menuBookGenerate);
+
+		// --- Menu affichage
+		Menu menuShow = new Menu("Affichage");
+		CheckMenuItem menuShowItemsCharacters = new CheckMenuItem("Items et personnage");
+		CheckMenuItem menuShowStats = new CheckMenuItem("Stats");
+		menuShowItemsCharacters.setSelected(true);
+		menuShowStats.setSelected(true);
+
+		menuShow.getItems().addAll(menuShowItemsCharacters, menuShowStats);
+
+		menuBar.getMenus().addAll(menuFile, menuBook, menuShow);
+
+		return menuBar;
+	}
+
 	private Node createLeftPanel() {
 		ToggleButton selectToogle = createToggleButton("Selectionner", Mode.SELECT);
-		selectToogle.setSelected(true);
-		this.mode = Mode.SELECT;
 		ToggleButton addNodeToggle = createToggleButton("Ajouter noeud", Mode.ADD_NODE);
 		ToggleButton addNodeLinkToggle = createToggleButton("Ajouter lien", Mode.ADD_NODE_LINK);
+		
+		selectToogle.setSelected(true);
+		this.mode = Mode.SELECT;
 
 		FlowPane flow = new FlowPane();
 		flow.getChildren().addAll(selectToogle, addNodeToggle, addNodeLinkToggle);
 		flow.setPadding(new Insets(5, 5, 5, 5));
-		
+
 		return flow;
 	}
-	
+
 	private ToggleButton createToggleButton(String text, Mode mode) {
 		ToggleButton toggleButton = new ToggleButton(text);
-		
+
 		toggleButton.setOnAction((ActionEvent e) -> {
 			MainWindow.this.mode = mode;
 		});
-		
+
 		toggleButton.setPrefSize(100, 100);
-		
-		if(this.toggleGroup == null) {
+
+		if (this.toggleGroup == null) {
 			this.toggleGroup = new ToggleGroup();
 		}
-		
+
 		this.toggleGroup.getToggles().add(toggleButton);
-		
+
 		return toggleButton;
 	}
-	
+
 	private void createANode() {
-	
+
 	}
 }
