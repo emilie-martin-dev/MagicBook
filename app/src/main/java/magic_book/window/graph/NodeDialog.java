b2795dbb25d17097f8d86beddb6b3c4eac56c57c
aure <21806986@unicaen.fr>
2020-02-17 22:31:17 +0100

    feat : ajout class mere Node

--- app/src/main/java/magic_book/window/graph/NodeDialog.java
+++ app/src/main/java/magic_book/window/graph/NodeDialog.java
@@ -3,111 +3,73 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package magic_book.window.graph;
+ package magic_book.window.graph;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.layout.HBox;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
+ import javafx.event.ActionEvent;
+ import javafx.event.EventHandler;
+ import javafx.application.Application;
+ import javafx.geometry.Insets;
+ import javafx.geometry.Pos;
+ import javafx.scene.Scene;
+ import javafx.scene.control.Button;
+ import javafx.scene.control.TextArea;
+ import javafx.scene.control.Label;
+ import javafx.scene.layout.GridPane;
+ import javafx.scene.control.ChoiceBox;
+ import javafx.scene.layout.HBox;
+ import javafx.stage.Modality;
+ import javafx.stage.Stage;
 
-import magic_book.core.node.Node;
-import magic_book.core.node.NodeType;
+ import magic_book.core.node.Node;
+ import magic_book.core.node.NodeType;
 
-public class NodeDialog extends Stage {
+ public class NodeDialog extends NodeMere {
 
-	private Node node = null;
-	private TextArea histoire;
-	private ChoiceBox nodeType;
+ 	private Node node = null;
+ 	private ChoiceBox nodeType;
 
 
-	public NodeDialog() {
-		initStageUI("Creation d'une page");
-		
-		this.showAndWait();
-	}
+ 	public NodeDialog() {
+ 		initStageUI("Creation d'une page");
+
+ 		this.showAndWait();
+ 	}
+
+ 	public NodeDialog(Node node) {
+ 		initStageUI("Edition de la page");
+ 		texte.setText(node.getText());
+ 		nodeType.setValue(node.getNodeType());
+
+ 		this.showAndWait();
+ 	}
 	
-	public NodeDialog(Node node) {
-		initStageUI("Edition de la page");
-		histoire.setText(node.getText());
-		nodeType.setValue(node.getNodeType());
-		
-		this.showAndWait();
-	}
-	
-	public void initStageUI(String title){
-		GridPane root = new GridPane();
-		root.setPadding(new Insets(25));
-
-		root.setHgap(10);
-		root.setVgap(10);
-
-		Label labelTexte = new Label("Texte :");
-		
-		histoire = new TextArea();
-		
+	@Override
+	public void ajout(GridPane root, Button boutonValider){
 		Label labelChoix = new Label("Choix du type du noeud :");
-
+		root.add(labelChoix, 0, 2);
+		
 		nodeType = new ChoiceBox();
 
-		nodeType.getItems().add(NodeType.BASIC);
-		nodeType.getItems().add(NodeType.VICTORY);
-		nodeType.getItems().add(NodeType.FAILURE);
-		nodeType.setValue(NodeType.BASIC);
+ 		nodeType.getItems().add(NodeType.BASIC);
+ 		nodeType.getItems().add(NodeType.VICTORY);
+ 		nodeType.getItems().add(NodeType.FAILURE);
+ 		nodeType.setValue(NodeType.BASIC);
+
+ 		HBox choix = new HBox(nodeType);
+		root.add(choix, 1, 2);
 		
-		HBox choix = new HBox(nodeType);
-
-		Button boutonAnnuler = new Button("Annuler");
-
-		boutonAnnuler.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				close();
-			}
-		});
-
-		Button boutonValider = new Button("Valider");
-
 		boutonValider.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
-				String texteHistoire = (String) histoire.getText();
+				String texteHistoire = (String) texte.getText();
 				NodeType choixBox = (NodeType) nodeType.getValue();
 				NodeDialog.this.node = new Node(texteHistoire, choixBox, null);
 				close();
 			}
 		});
-
-		root.add(labelTexte, 0, 0);
-		root.add(histoire, 0, 1, 2, 1);
-		root.add(labelChoix, 0, 2);
-		root.add(choix, 1, 2);
-		
-		HBox box = new HBox();
-		box.setSpacing(10d);
-		box.setAlignment(Pos.BASELINE_RIGHT);
-		box.getChildren().add(boutonAnnuler);
-		box.getChildren().add(boutonValider);
-		root.add(box, 0, 3, 2, 1);
-		
-		Scene scene = new Scene(root);
-		this.initModality(Modality.APPLICATION_MODAL);
-		this.setResizable(false);
-		this.setTitle(title);
-		this.setScene(scene);
 	}
-
+	
 	public Node getNode() {
-		return this.node;
+		return node;
 	}
-
-}
+ }
--- app/src/main/java/magic_book/window/graph/NodeLinkDialog.java
+++ app/src/main/java/magic_book/window/graph/NodeLinkDialog.java
@@ -3,91 +3,53 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package magic_book.window.graph;
+ package magic_book.window.graph;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
+ import javafx.event.ActionEvent;
+ import javafx.event.EventHandler;
+ import javafx.geometry.Insets;
+ import javafx.geometry.Pos;
+ import javafx.scene.Scene;
+ import javafx.scene.control.Button;
+ import javafx.scene.control.ChoiceBox;
+ import javafx.scene.control.Label;
+ import javafx.scene.control.TextArea;
+ import javafx.scene.layout.GridPane;
+ import javafx.scene.layout.HBox;
+ import javafx.stage.Modality;
+ import javafx.stage.Stage;
 
-import magic_book.core.node.Node;
-import magic_book.core.node.NodeLink;
-import magic_book.core.node.NodeType;
+ import magic_book.core.node.Node;
+ import magic_book.core.node.NodeLink;
+ import magic_book.core.node.NodeType;
 
-/**
- *
- * @author norah
- */
-public class NodeLinkDialog extends Stage {
-	
-	private TextArea choix;
-	
-	public NodeLinkDialog() {
-		initStageUI("Création du texte de choix");
-		this.showAndWait();
+ /**
+  *
+  * @author norah
+  */
+ public class NodeLinkDialog extends NodeMere{
+
+ 	public NodeLinkDialog() {
+ 		initStageUI("Création du texte de choix");
+ 		this.showAndWait();
+ 	}
+
+ 	public NodeLinkDialog(NodeLink nodeLink) {
+ 		initStageUI("Modification du texte de choix");
+ 		this.showAndWait();
+ 	}
+
+	@Override
+	public void ajout(GridPane root, Button boutonValider){
+ 		boutonValider.setOnAction(new EventHandler<ActionEvent>() {
+ 			@Override
+ 			public void handle(ActionEvent e) {
+ 				String texteHistoire = (String) texte.getText();
+ 				//NodeType choixBox = (NodeType) choix.getValue();
+ 				//NodeDialog.this.node = new Node(texteHistoire, choixBox, null);
+ 				close();
+ 			}
+ 		});
 	}
-	
-	public NodeLinkDialog(NodeLink nodeLink) {
-		initStageUI("Modification du texte de choix");
-		this.showAndWait();
-	}
-	
-	public void initStageUI(String title){
-		GridPane root = new GridPane();
-		root.setPadding(new Insets(25));
 
-		root.setHgap(10);
-		root.setVgap(10);
-
-		Label labelTexte = new Label("Ajouter le choix");
-		
-		choix = new TextArea();
-
-		Button boutonAnnuler = new Button("Annuler");
-
-		boutonAnnuler.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				close();
-			}
-		});
-
-		Button boutonValider = new Button("Valider");
-
-		boutonValider.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				String texteHistoire = (String) choix.getText();
-				//NodeType choixBox = (NodeType) choix.getValue();
-				//NodeDialog.this.node = new Node(texteHistoire, choixBox, null);
-				close();
-			}
-		});
-
-		root.add(labelTexte, 0, 0);
-		root.add(choix, 0, 1);
-		
-		HBox box = new HBox();
-		box.setSpacing(10d);
-		box.setAlignment(Pos.BASELINE_RIGHT);
-		box.getChildren().add(boutonAnnuler);
-		box.getChildren().add(boutonValider);
-		root.add(box, 0, 3, 2, 1);
-		
-		Scene scene = new Scene(root);
-		this.initModality(Modality.APPLICATION_MODAL);
-		this.setResizable(false);
-		this.setTitle(title);
-		this.setScene(scene);
-	}
-	
-}
+ }
