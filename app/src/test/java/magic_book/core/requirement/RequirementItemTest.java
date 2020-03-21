aabc105c5b2d1238149d08f1fbef3a218daac85b
aure <21806986@unicaen.fr>
2020-03-21 16:08:55 +0100

    feat : ajout de class test pour les Requirement Item et Skill

---
+++ app/src/test/java/magic_book/core/requirement/RequirementItemTest.java
@@ -0,0 +1,23 @@
+package magic_book.core.requirement;
+
+import magic_book.core.game.BookCharacter;
+import magic_book.core.game.BookState;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RequirementItemTest {
+	@Test
+	public void isStatisfied(){
+		BookCharacter bookCharacter = new BookCharacter("", "", 0, 0, null, null, 0);
+		bookCharacter.addItem("cle");
+
+		BookState bookState = new BookState();
+		bookState.setMainCharacter(bookCharacter);
+
+		AbstractRequirement requirementOwn = new RequirementItem("cle");
+		AbstractRequirement requirementNotOwn = new RequirementItem("hache");
+
+		Assert.assertTrue(requirementOwn.isSatisfied(bookState));
+		Assert.assertFalse(requirementNotOwn.isSatisfied(bookState));
+	}
+}
---
+++ app/src/test/java/magic_book/core/requirement/RequirementSkillTest.java
@@ -0,0 +1,24 @@
+package magic_book.core.requirement;
+
+import magic_book.core.game.BookCharacter;
+import magic_book.core.game.BookState;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RequirementSkillTest {
+	
+	@Test
+	public void isStatisfied(){
+		BookCharacter bookCharacter = new BookCharacter("", "", 0, 0, null, null, 0);
+		bookCharacter.addSkill("eclair");
+
+		BookState bookState = new BookState();
+		bookState.setMainCharacter(bookCharacter);
+
+		AbstractRequirement requirementOwn = new RequirementSkills("eclair");
+		AbstractRequirement requirementNotOwn = new RequirementSkills("feu");
+
+		Assert.assertTrue(requirementOwn.isSatisfied(bookState));
+		Assert.assertFalse(requirementNotOwn.isSatisfied(bookState));
+	}
+}
