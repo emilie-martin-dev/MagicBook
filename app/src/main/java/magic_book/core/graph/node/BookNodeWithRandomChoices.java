7ee8878c64a84b3931176c6c774ac34b08685af4
aure <21806986@unicaen.fr>
2020-03-21 15:55:54 +0100

    feat : BookNodeWithRandomChoice: ajout de la méthode getRandomChoices permettant de choisir par rapport au chance du noeud

--- app/src/main/java/magic_book/core/graph/node/BookNodeWithRandomChoices.java
+++ app/src/main/java/magic_book/core/graph/node/BookNodeWithRandomChoices.java
@@ -1,6 +1,9 @@
 package magic_book.core.graph.node;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import magic_book.core.game.BookState;
 import magic_book.core.item.BookItemLink;
 import magic_book.core.graph.node_link.BookNodeLinkRandom;
 
@@ -12,5 +15,32 @@ public class BookNodeWithRandomChoices extends AbstractBookNodeWithChoices<BookN
 	
 	public BookNodeWithRandomChoices(String text, Integer nbItemsAPrendre, List<BookItemLink> itemLinks, List<BookItemLink> shopItemLinks, List<BookNodeLinkRandom> choices) {
 		super(text, nbItemsAPrendre, itemLinks, shopItemLinks, choices);
+	}
+	
+	public BookNodeLinkRandom getRandomChoices(BookState state){
+		List<BookNodeLinkRandom> listNodeLinkDisponible = new ArrayList();
+		int somme = 0;
+		int nbrChoice = 0;
+		for (int i = 0 ; i < this.getChoices().size() ; i++){
+			if(this.getChoices().get(i).isAvailable(state)){
+				listNodeLinkDisponible.add(this.getChoices().get(i));
+				somme += this.getChoices().get(i).getChance();
+			}
+		}
+		if(listNodeLinkDisponible.isEmpty()){
+			BookNodeTerminal bookNodeTerminalFail = new BookNodeTerminal("Dommage.. Vous êtes mort", BookNodeStatus.FAILURE);
+			BookNodeLinkRandom bookNodeLinkTerminal = new BookNodeLinkRandom("C'est la voie de la raison", bookNodeTerminalFail, null, 0);
+			return bookNodeLinkTerminal;
+		} else {
+			Random random = new Random();
+			int nbrRandomChoice = random.nextInt(somme);
+			for (int i = 0 ; i < listNodeLinkDisponible.size() ; i++){
+				nbrRandomChoice -= this.getChoices().get(i).getChance();
+				if(nbrRandomChoice <= 0)
+					nbrChoice = i;
+					break;
+			}
+		}
+		return this.getChoices().get(nbrChoice) ;
 	}
 }
