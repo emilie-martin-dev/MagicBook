86a674040fb19fd680fadf379fa06be7981d4c5d
aure <21806986@unicaen.fr>
2020-03-30 17:17:08 +0200

    feat : ajout de gain/perte de hp, gold + choiceBox pour auto dans les NodeLinkDialog

--- app/src/main/java/magic_book/window/dialog/NodeLinkDialog.java
+++ app/src/main/java/magic_book/window/dialog/NodeLinkDialog.java
@@ -3,6 +3,7 @@ package magic_book.window.dialog;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -19,8 +20,15 @@ public class NodeLinkDialog extends AbstractDialog{
 	private BookNodeLink nodeLink;
 	
 	private TextField chanceTextField;
+	private TextField hpTextField;
+	private TextField goldTextField;
 	private Label chanceLabel;
+	private Label hpLabel;
+	private Label goldLabel;
+	private Label autoLabel;
 	
+	private ChoiceBox autoBox;
+
 	private AbstractBookNode firstNode;
 	
 	private GridPane root;
@@ -33,6 +41,14 @@ public class NodeLinkDialog extends AbstractDialog{
 			root.add(chanceLabel, 0, 2);
 			root.add(chanceTextField, 1, 2);
 			this.nodeLink = nodeLinkRandom;
+		} else {
+			root.add(hpLabel, 0, 2);
+			root.add(hpTextField, 1, 2);
+			root.add(goldLabel, 0, 3);
+			root.add(goldTextField, 1, 3);
+			root.add(autoLabel, 0, 4);
+			root.add(autoBox, 1, 4);
+			autoBox.setValue(true);
 		}
 		this.showAndWait();
 	}
@@ -46,7 +62,15 @@ public class NodeLinkDialog extends AbstractDialog{
 			chanceTextField.setText(String.valueOf(nodeLinkRandom.getChance()));
 			this.nodeLink = nodeLinkRandom;
 		} else {
-			this.nodeLink = nodeLink;
+			root.add(hpLabel, 0, 2);
+			root.add(hpTextField, 1, 2);
+			root.add(goldLabel, 0, 3);
+			root.add(goldTextField, 1, 3);
+			root.add(autoLabel, 0, 4);
+			root.add(autoBox, 1, 4);
+			hpTextField.setText(String.valueOf(nodeLink.getHp()));
+			goldTextField.setText(String.valueOf(nodeLink.getGold()));
+			autoBox.setValue(String.valueOf(nodeLink.getAuto()));
 		}
 		texte.setText(nodeLink.getText());	
 		
@@ -63,10 +87,20 @@ public class NodeLinkDialog extends AbstractDialog{
 		Label textLabel = new Label("Texte choix :");
 		texte = new TextArea();
 		texte.setWrapText(true);
-		
 
 		chanceLabel = new Label("Chance pour aller dans vers ce lien");
 		chanceTextField = new TextField();
+		
+		hpLabel = new Label("hp (gain ou perte)");
+		hpTextField = new TextField();
+		
+		goldLabel = new Label("gold (gain ou perte)");
+		goldTextField = new TextField();
+		
+		autoLabel = new Label("auto");
+		autoBox = new ChoiceBox<>();
+		autoBox.getItems().add(true);
+		autoBox.getItems().add(false);
 		
 		root.add(textLabel, 0, 0);
 		root.add(texte, 0, 1, 2, 1);
@@ -94,7 +128,18 @@ public class NodeLinkDialog extends AbstractDialog{
 					}
 				}
 				else{
-					NodeLinkDialog.this.nodeLink = new BookNodeLink(texteHistoire, -1);
+					if(hpTextField.getText().isEmpty()||goldTextField.getText().isEmpty()){
+						return;
+					}
+					try {
+						int hpInt = Integer.parseInt(hpTextField.getText());
+						int goldInt = Integer.parseInt(goldTextField.getText());
+						NodeLinkDialog.this.nodeLink = new BookNodeLink(texteHistoire, -1, null, hpInt, goldInt, (boolean) autoBox.getValue());
+					} catch (NumberFormatException ex){
+						notANumberAlertDialog(ex);
+						return;
+					}
+					
 				}
 			} else {
 					
@@ -106,6 +151,19 @@ public class NodeLinkDialog extends AbstractDialog{
 						int chanceInt = Integer.parseInt(chanceTextField.getText());
 						BookNodeLinkRandom nodeLinkRandom = (BookNodeLinkRandom) nodeLink;
 						nodeLinkRandom.setChance(chanceInt);
+					} catch (NumberFormatException ex){
+						notANumberAlertDialog(ex);
+						return;
+					}
+				} else {
+					if (hpTextField.getText().isEmpty()||goldTextField.getText().isEmpty()){
+						return;
+					} 
+					try {
+						int hpInt = Integer.parseInt(hpTextField.getText());
+						int goldInt = Integer.parseInt(goldTextField.getText());
+						NodeLinkDialog.this.nodeLink.setGold(goldInt);
+						NodeLinkDialog.this.nodeLink.setHp(hpInt);
 					} catch (NumberFormatException ex){
 						notANumberAlertDialog(ex);
 						return;
